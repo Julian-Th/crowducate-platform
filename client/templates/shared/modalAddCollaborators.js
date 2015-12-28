f4bc77d5ea279358116303fbaeb6dc0eea50fde2
Amir Rahbaran <amirrahbaran@hotmail.com>
2015-12-28 23:08:01 +0100

    remove collaborator (not working)

--- client/templates/shared/modalAddCollaborators.js
+++ client/templates/shared/modalAddCollaborators.js
@@ -8,21 +8,35 @@ Template.modalAddCollaborators.courses = function(){
  //return users.find().fetch().map(function(it){ return it.username; });
 };
 
-Template.modalAddCollaborators.events({
-	'click #js-addCollaborator' : function (event) {
-		var collaboratorName = $('#collaboratorName').val(); // 
-		Courses.update(
-		   { _id: this._id },
-		   { $addToSet: {canEditCourse: collaboratorName } }
-		)
-		$('#collaboratorName').val("");
-	}
-});
-
 Template.modalAddCollaborators.helpers({
 	'addedCollaborators': function () {
 		return Courses.find().fetch();
-		//return Courses.find({_id: this._id}, {$in: "canEditCourse"});
-		//return Courses.distinct("canEditCourse");
 	}
 });
+
+Template.modalAddCollaborators.events({
+	'click #js-addCollaborator' : function (event) {
+		var collaboratorName = $('#collaboratorName').val();
+		Courses.update(
+		   {_id: this._id},
+		   {$addToSet: {canEditCourse: collaboratorName}}
+		);
+		$('#collaboratorName').val("");
+	},
+	'click #remove-collaborator': function (event) {
+		console.log(event.target);
+		console.log($(event.target).parent());
+		var listedCollaborator = $(event.target).parent().text();
+		//console.log(listedCollaborator);
+		// Courses.update(
+		// 	{_id: this._id }, 
+		// 	{$pull: {canEditCourse: listedCollaborator}}
+		// );
+	}
+});
+
+// Template.nestedTemplate.events({
+//   'click #remove-collaborator': function (event) {
+//     Courses.update({_id: Template.parentData()._id },{$pull: {canEditCourse: this}});
+//   }
+// });
